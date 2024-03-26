67f0b4ed752ba19a1ba9bc088e93ffb20ebf701b
Edwin Moquete <edwinmmoquete@gmail.com>
2024-03-26 16:55:59 +0100

    input: Bring back PointerProperties::copyFrom()
    libwfdnative compiled for 14 QPR1 depends on this symbol.
    
    Change-Id: I7bc5ee3fb7760a9f470023cd0daa215b2e194dc1
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- include/input/Input.h
+++ include/input/Input.h
@@ -576,6 +576,8 @@ struct PointerProperties {
     }
 
     PointerProperties& operator=(const PointerProperties&) = default;
+
+    void copyFrom(const PointerProperties& other);
 };
 
 std::ostream& operator<<(std::ostream& out, const PointerProperties& properties);
