495045e624774b864b80a2406729266db99682e0
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-05-01 08:39:26 +0800

    use low ram dalvik config for 6gb devices
    Change-Id: I677cdf3f90a479179c60d20ed3f764a420a36962

--- build/phone-xhdpi-6144-dalvik-heap.mk
+++ build/phone-xhdpi-6144-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a 6GB phone
 
 PRODUCT_VENDOR_PROPERTIES += \
-    dalvik.vm.heapstartsize?=16m \
-    dalvik.vm.heapgrowthlimit?=256m \
+    dalvik.vm.heapstartsize?=8m \
+    dalvik.vm.heapgrowthlimit?=192m \
     dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.75 \
-    dalvik.vm.heapminfree?=512k \
-    dalvik.vm.heapmaxfree?=8m
+    dalvik.vm.heaptargetutilization?=0.6 \
+    dalvik.vm.heapminfree?=8m \
+    dalvik.vm.heapmaxfree?=16m
